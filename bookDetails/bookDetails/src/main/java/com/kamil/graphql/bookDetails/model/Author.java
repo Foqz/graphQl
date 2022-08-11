1ba65e0c09a52060c067a5c708d4f98e7ed6973b
kamil.fiks <kamil.fiks@canpack.com>
2022-08-11 12:47:18 +0200

    Classes for Author And Book and basic graphQL schemas

---
+++ bookDetails/bookDetails/src/main/java/com/kamil/graphql/bookDetails/model/Author.java
@@ -0,0 +1,30 @@
+package com.kamil.graphql.bookDetails.model;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Author {
+    private String id;
+    private String firsName;
+    private String lastName;
+
+    public Author(String id, String firsName, String lastName) {
+        this.id = id;
+        this.firsName = firsName;
+        this.lastName = lastName;
+    }
+
+    private static List<Author> authors = Arrays.asList(
+            new Author("author-1", "Joanne", "Rowling"),
+            new Author("author-2", "Herman", "Melville"),
+            new Author("author-3", "Anne", "Rice")
+    );
+
+    public static Author getById(String id) {
+        return authors.stream().filter(author -> author.getId().equals(id)).findFirst().orElse(null);
+    }
+
+    public String getId() {
+        return id;
+    }
+}
